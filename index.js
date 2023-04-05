6916cdb8de762789247fb927d40d16d0f9ad38fe
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2023-04-05 23:43:00 +0530

    lar

--- index.js
+++ index.js
@@ -52,12 +52,7 @@ mongoose.connect(mongoURI, {
      
     console.log('Connected to  congo database');
   })
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
+  
 app.get('/', (req, res) => {
 
     res.send('Hello World Aniket boss!')
@@ -372,6 +367,7 @@ app.post('/login', async (req, res) => {
         })
     }
     const token = generateToken(user);
+    res.setHeader('Access-Control-Allow-Origin', 'https://api-nirog.vercel.app');
   res.send({
     message:"login success",
     status:"200",
